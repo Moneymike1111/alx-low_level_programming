db1b3d6962423583055051323d39ee74788e6351
Moneymike1111 <israelgette@yahoo.com>
2024-01-30 01:47:40 -0800

    new entry

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - a function that give the absolute value of an int
+ *
+ * @kutr: argument for the function
+ *
+ * Return: always a positive integer
+ *
+ */
+int _abs(int kutr)
+{
+	if (kutr >= 0)
+	{
+		return (kutr);
+	}
+	else
+	{
+		kutr *= -1
+		return (kutr);
+	}
+}
